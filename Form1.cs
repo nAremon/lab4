0b05788fdc48cf5f0e3b8903fd1a0a99300089bd
nAremon <104908611+nAremon@users.noreply.github.com>
2022-05-04 17:41:30 +0300

    lab4
    lab4

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double Dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = Convert.ToDouble(textBox6.Text);
+            double c = Convert.ToDouble(textBox7.Text);
+            double x = x0;
+            while ( x <=xk)
+            {
+                double y = (0.01 * b * c) / x + Math.Cos(Math.Sqrt(x * a * a * a));
+                textBox5.Text += "x=" + Convert.ToString(x) + "; y =" + Convert.ToString(y) + Environment.NewLine;
+                x = x + Dx;
+            }
+        }
+    }
+}
